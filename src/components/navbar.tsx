aeef615fd27e6936eef8ea107d158763d148dfee
lnirjar <36673870+lnirjar@users.noreply.github.com>
2025-03-25 14:50:44 +0530

    fix: refactored navbar code

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,48 +3,44 @@
 import { UserButton } from "@clerk/nextjs";
 import { usePathname } from "next/navigation";
 
+import { useWorkspaceId } from "@/app/(dashboard)/workspaces/hooks/use-workspace-id";
 import { MobileSidebar } from "@/components/mobile-sidebar";
 import { Sidebar } from "@/components/sidebar";
 
-const navItems = [
-  {
-    path: "workspaces",
-    title: "Workspaces",
-    description: "Browse and access all your workspaces in one place.",
-  },
-  {
-    path: "home",
-    title: "Home",
-    description: "View an overview of your workspaces, projects, and tasks.",
-  },
-  {
-    path: "tasks",
-    title: "Tasks",
-    description: "Manage and track your tasks across different projects.",
-  },
-  {
-    path: "settings",
-    title: "Settings",
-    description: "Update workspace settings, preferences, and configurations.",
-  },
-  {
-    path: "members",
-    title: "Members",
-    description: "View and manage workspace members and permissions.",
-  },
-];
-
 export const Navbar = () => {
   const pathname = usePathname();
-  const pathnameArr = pathname.split("/");
-  const path =
-    pathnameArr.length === 4
-      ? pathnameArr[3]
-      : pathnameArr.length === 3
-      ? "home"
-      : "workspaces";
+  const workspaceId = useWorkspaceId();
 
-  const navItem = navItems.find((item) => item.path === path);
+  const navItems = [
+    {
+      path: `/workspaces`,
+      title: "Workspaces",
+      description: "Browse and access all your workspaces in one place.",
+    },
+    {
+      path: `/workspaces/${workspaceId}`,
+      title: "Home",
+      description: "View an overview of your workspaces, projects, and tasks.",
+    },
+    {
+      path: `/workspaces/${workspaceId}/tasks`,
+      title: "Tasks",
+      description: "Manage and track your tasks across different projects.",
+    },
+    {
+      path: `/workspaces/${workspaceId}/settings`,
+      title: "Settings",
+      description:
+        "Update workspace settings, preferences, and configurations.",
+    },
+    {
+      path: `/workspaces/${workspaceId}/members`,
+      title: "Members",
+      description: "View and manage workspace members and permissions.",
+    },
+  ];
+
+  const navItem = navItems.find((item) => item.path === pathname);
 
   return (
     <nav className="pt-4 px-6 flex items-center justify-between">
