3c6c3816cb0bf6131608b7d53ff63142394bfbe4
lnirjar <36673870+lnirjar@users.noreply.github.com>
2025-11-11 01:40:18 +0530

    added favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="apple-mobile-web-app-title" content="Issue Tracker" />
+      </head>
       <body className={cn(geistSans.className, "antialiased min-h-screen")}>
         <ClerkProvider>
           <QueryProvider>
